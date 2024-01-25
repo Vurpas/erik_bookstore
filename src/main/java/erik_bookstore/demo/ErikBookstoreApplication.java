847ece1fc494e45fd17d5d27c043df3f0c90f418
Vurpas <erikengstrom164@gmail.com>
2024-01-25 14:22:44 +0100

    first commit

---
+++ src/main/java/erik_bookstore/demo/ErikBookstoreApplication.java
@@ -0,0 +1,13 @@
+package erik_bookstore.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErikBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErikBookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/erik_bookstore/demo/ErikBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package erik_bookstore.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErikBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
