7baf9e4bd8324bfca1a4e6696bdb09c56ef552d6
Vurpas <erikengstrom164@gmail.com>
2024-01-26 08:28:25 +0100

    Added create book schemas

--- src/main/java/erik_bookstore/demo/controllers/BookController.java
+++ src/main/java/erik_bookstore/demo/controllers/BookController.java
@@ -1,4 +1,26 @@
 package erik_bookstore.demo.controllers;
 
+import erik_bookstore.demo.models.Book;
+import erik_bookstore.demo.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/books")
 public class BookController {
+    @Autowired
+    BookService bookService;
+
+    // POST create new book
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        return bookService.createBook(book);
+    }
+
+
+
+
 }
--- src/main/java/erik_bookstore/demo/services/BookService.java
+++ src/main/java/erik_bookstore/demo/services/BookService.java
@@ -1,4 +1,19 @@
 package erik_bookstore.demo.services;
 
+import erik_bookstore.demo.models.Book;
+import erik_bookstore.demo.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService {
+    @Autowired
+    BookRepository bookRepository;
+
+    public Book createBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+
+
 }
