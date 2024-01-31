058d8bc778263775f07cbae303f59d080c2caea6
Vurpas <erikengstrom164@gmail.com>
2024-01-31 09:46:06 +0100

    created LOAN model

--- src/main/java/erik_bookstore/demo/models/Loan.java
+++ src/main/java/erik_bookstore/demo/models/Loan.java
@@ -1,4 +1,41 @@
 package erik_bookstore.demo.models;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Document(collection = "loans")
 public class Loan {
+    @Id
+    private String id;
+    private String book_id;
+    private String loanedByUserId;
+    @CreatedDate
+    private Date date_of_loan;
+    private LocalDate return_date = LocalDate.now().plusDays(14);
+
+    public Loan () {
+    }
+    public String getId() {
+        return id;
+    }
+    public String getLoanedByUserId() {
+        return loanedByUserId;
+    }
+    public Date getDate_of_loan() {
+        return date_of_loan;
+    }
+    public LocalDate getReturn_Date() {
+        return return_date;
+    }
+    public String getBook_id() {
+        return book_id;
+    }
+
+    public void setReturn_date(LocalDate return_date) {
+        this.return_date = return_date;
+    }
 }
